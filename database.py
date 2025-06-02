9bc9917a548a7cf5c786e4a8df87ab4179f8e7e9
70007658 <70007658@krungsri.com>
2025-06-02 20:32:34 +0700

    Add file env and fix code database

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
+import os
+from dotenv import load_dotenv  # <-- ถ้ายังไม่มี
+
+load_dotenv()  # โหลดจากไฟล์ .env
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 # DATABASE_URL = "postgresql://postgres:yeeStock15@eipmouvlllqdagdfqrhv.supabase.co:5432/postgres"
 
-DATABASE_URL = (
-    "postgresql://postgres.eipmouvlllqdagdfqrhv:yeeStock15"
-    "@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
-    "?options=--cluster%3Ddb-eipmouvlllqdagdfqrhv&sslmode=require&gssencmode=disable"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL,connect_args={
     "sslmode": "require",
