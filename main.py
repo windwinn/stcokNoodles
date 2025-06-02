7286c2f93e5b7f4a955d5e5b69163fb8a5df5454
70007658 <70007658@krungsri.com>
2025-06-02 21:21:44 +0700

    test front

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://stocknoodles.pages.dev/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
