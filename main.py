85d307a79d099c47c0233f86c1a2f41dcba6116f
70007658 <70007658@krungsri.com>
2025-06-03 22:02:46 +0700

    fix ui line

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def broadcast_line(stock_id: int, db: Session = Depends(get_db)):
     for p in stock.products:
         message_lines.append(f"- {p.name} : {p.order} {p.order_unit}")
         if p.name == 'กากหมู':
+            message_lines.append(f"\n")
             message_lines.append(f"🥬 ผัก \n")
     message = "\n".join(message_lines)
     token = 'PCQYVri8Sj3x7ut1sdEiGNUbXfI4/2kEQ7i+w+ggGJ5A+cCe/t4rySQvBxux8E/yQIoMDisMykSeLG/R17B7QUIkGBzs3yciD+vRzM642T43IcXC+12GzMT0AjE6ZV1h0Yv8VU0ALCvVn/N/5S8xhAdB04t89/1O/w1cDnyilFU='
